99e511a40f7b7c11aabd454e5122fe2a8c162b64
Lonardo <silvaleonardo1899@gmail.com>
2026-09-24 17:07:57 -0300

    Add files via upload

---
+++ ex01.js/ex01.js
@@ -0,0 +1,24 @@
+//declare uma variavel nome e imprima o valor na tela//
+
+let nome = "Leonardo"
+console.log(nome)
+
+//dado o codigo abaixo, aponte porque e gerado erro e corrija//
+const idade1 = 16
+idade = 20
+
+// o codigo nao funciona porque nao da para mudar a variavel com const, deve ser let para mudar
+
+let idade2 = 16
+idade = 20
+console.log(idade2)
+
+//Crie uma constante PI com com valor 3,14 e uma variavel raio com valor 5. Calcule a area de um circulo e exiba o resutado
+
+const PI = 3.14
+const raio = 5
+const soma = PI * raio * raio 
+
+console.log(soma)
+
+//
